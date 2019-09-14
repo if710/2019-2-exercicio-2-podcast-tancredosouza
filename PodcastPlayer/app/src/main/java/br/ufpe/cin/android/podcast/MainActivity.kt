9c2dc221a09ec3ad0b879e1972116b4eb9c2e002
Tantan <tancredo.souza@inloco.com.br>
2019-09-14 08:55:39 -0300

    feature(PodcastPlayer): add initial version of RecyclerView

--- PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
+++ PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
@@ -1,29 +1,36 @@
 package br.ufpe.cin.android.podcast
 
+import android.os.AsyncTask
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.itemlista.*
+import androidx.recyclerview.widget.LinearLayoutManager
+import br.ufpe.cin.android.podcast.adapters.ItemFeedAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.sdk27.coroutines.onClick
 import java.net.URL
 
 
 class MainActivity : AppCompatActivity() {
+    var itemFeeds: List<ItemFeed>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.itemlista)
+        setContentView(R.layout.activity_main)
 
-        item_action.onClick {
-            doAsync {
-                val xmlDownloadLink =
-                    "https://s3-us-west-1.amazonaws.com/podcasts.thepolyglotdeveloper.com/podcast.xml"
+        var itemFeeds: List<ItemFeed>
 
-                val rssFeed = URL(xmlDownloadLink).readText()
+        doAsync {
+            val xmlDownloadLink =
+                "https://s3-us-west-1.amazonaws.com/podcasts.thepolyglotdeveloper.com/podcast.xml"
 
-                val itemFeed = Parser.parse(rssFeed)
+            val rssFeed = URL(xmlDownloadLink).readText()
 
-                println(itemFeed.size)
+            itemFeeds = Parser.parse(rssFeed)
+
+            listRecyclerView.apply {
+                layoutManager = LinearLayoutManager(this@MainActivity)
+
+                adapter = ItemFeedAdapter(itemFeeds, this@MainActivity)
             }
         }
     }
